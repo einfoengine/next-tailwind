d0c48d9072882564b0665c1a83dd9bb06b52923e
Mohammad Shanewaz Al Maruf <einfoengine@gmail.com>
2022-07-20 19:48:32 +0600

    Server setup

---
+++ server/app.js
@@ -0,0 +1,19 @@
+// const express = require('express')
+import express from 'express';
+// import cors from 'cors'
+
+// const morgan = require('morgan');
+require("dotenv").config();
+
+const app = express();
+
+
+app.get('/',(req,res)=>{
+    res.send('This is running!')
+})
+
+
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log(`The pakhi bhai server is running at port ${port}`);
+})
