4b8af7da58df1009f17833b146c1eb759fe0efc2
Mohammad Shanewaz Al Maruf <einfoengine@gmail.com>
2022-09-21 05:15:03 +0600

    Hydration error solved, Login component correction, Navigation Link component adopted

--- server/routs/api/users.js
+++ server/routs/api/users.js
@@ -1,17 +1,11 @@
 import express from "express";
 const router = express.Router();
 
-// Models
-
 
 // Conntrollers
-
 import {Register} from '../../controllers/register.js';
 
 // Requests
-router.get('/registration', (req, res)=>{
-    res.send("Registraion responded!")
-});
 
 router.post('/registration', Register);
 
